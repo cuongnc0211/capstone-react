dd734a4fa33a98d5df20444c8593bfc534c2dff8
cuongnc0211 <cuongnguyenfu@gmail.com>
2023-05-30 17:58:55 +0700

    add validation

--- src/pages/Reservation/ReservationForm.js
+++ src/pages/Reservation/ReservationForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRef } from 'react';
 
 const ReservationForm = (props) => {
@@ -6,9 +6,23 @@ const ReservationForm = (props) => {
   const timeRef = useRef(null);
   const guestsRef = useRef(null);
   const ocassionRef = useRef(null);
+  const [errors, setErrors] = useState({});
 
   const handleChangeDate = () => {
-    props.changeDate(dateRef.current.value)
+    const date = new Date(dateRef.current.value)
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    debugger;
+    if(date >= today ) {
+      props.changeDate(dateRef.current.value)
+      const {date: _, ...newErrors} = errors
+      setErrors(newErrors);
+    } else {
+      let newErrors = {...errors}
+      newErrors['date'] = 'Please do not choose past date.'
+      setErrors(newErrors);
+    }
   };
   const handleChangeTime = () => {
     props.changeTime(timeRef.current.value)
@@ -25,9 +39,10 @@ const ReservationForm = (props) => {
   }
 
   return (
-    <form style={{display: 'grid', maxWidth: 500, gap: 10, marginBottom: 20, marginTop: '30px'}}>
+    <form style={{display: 'grid', maxWidth: 500, gap: 10, marginBottom: 20, marginTop: '30px'}} onSubmit={handleSubmit}>
       <label className='label-control' htmlFor="res-date">Choose date</label>
-      <input className='form-control' type="date" ref={dateRef} id="res-date" onChange={handleChangeDate} />
+      <input className='form-control' type="date" ref={dateRef} id="res-date" onChange={handleChangeDate} required />
+      <p style={{color: 'red'}}>{ errors['date'] }</p>
       <br />
 
       <label className='label-control' htmlFor="res-time">Choose time</label>
@@ -42,7 +57,7 @@ const ReservationForm = (props) => {
       <br />
 
       <label className='label-control' htmlFor="guests">Number of guests</label>
-      <input className='form-control' type="number" placeholder={1} min={1} max={10} id="guests" ref={guestsRef} onChange={handleChangeGuests} />
+      <input className='form-control' type="number" required placeholder='input the guests number' min={1} max={10} id="guests" ref={guestsRef} onChange={handleChangeGuests} />
       <br />
 
       <label className='label-control' htmlFor="occasion">Occasion</label>
@@ -53,7 +68,14 @@ const ReservationForm = (props) => {
       </select>
       <br />
 
-      <input className='btn btn-success' type="submit" defaultValue="Make Your reservation" onClick={(e) => { handleSubmit(e) } } />
+      <button
+        type="submit"
+        className="btn btn-success"
+        disabled={Object.keys(errors).length > 0 ? true : undefined}
+      >
+        Make Your reservation
+      </button>
+
       <br />
     </form>
   );
